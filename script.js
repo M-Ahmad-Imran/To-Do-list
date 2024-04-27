00498a360cc80541939356dc30912ebc387262f0
M-Ahmad-Imran <mahmadimran1122@gmal.com>
2024-04-27 14:58:15 +0500

    Formated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function DT(){
+function DT() {
     let date = new Date();
     let year = date.getFullYear();
     let month = String(date.getMonth() + 1).padStart(2, 0);
@@ -9,30 +9,30 @@ function DT(){
     return `${year}-${month}-${day}T${hour}:${minutes}`
 }
 
-    document.getElementById("input-date").value = DT();
+document.getElementById("input-date").value = DT();
 
-    function formatDate(Datetime) {
-        let date = new Date(Datetime);
-        let year = date.getFullYear();
-        let month = String(date.getMonth()).padStart(2, 0);
-        let day = String(date.getDate()).padStart(2, 0);
-        let hour = String(date.getHours()).padStart(2, 0);
-        let minutes = String(date.getMinutes()).padStart(2, 0);
-        const dt = `Date:${day}-${month}-${year}<br>Time:${hour}:${minutes}`;
+function formatDate(Datetime) {
+    let date = new Date(Datetime);
+    let year = date.getFullYear();
+    let month = String(date.getMonth()).padStart(2, 0);
+    let day = String(date.getDate()).padStart(2, 0);
+    let hour = String(date.getHours()).padStart(2, 0);
+    let minutes = String(date.getMinutes()).padStart(2, 0);
+    const dt = `Date:${day}-${month}-${year}<br>Time:${hour}:${minutes}`;
 
-        return dt;
-    }
+    return dt;
+}
 
-    function addTask(tasks, date, description) {
-        const task = tasks;
-        const task_time = date;
-        const desc = description;
-        const formatedDate = formatDate(task_time);
+function addTask(tasks, date, description) {
+    const task = tasks;
+    const task_time = date;
+    const desc = description;
+    const formatedDate = formatDate(task_time);
 
-        if (task !== "" && task_time !== "" && desc !== "") {
-            const li = document.createElement('li');
+    if (task !== "" && task_time !== "" && desc !== "") {
+        const li = document.createElement('li');
 
-            li.innerHTML = `
+        li.innerHTML = `
         <h1 class="todotext">${task}</h1>
         <span class="datetime">${formatedDate}</span>
         <span class="tododesc">${desc}</span>
@@ -43,50 +43,50 @@ function DT(){
             <button id="safebtn">Safe</button>
         </span>`
 
-            const edittext = li.querySelector(".edittext");
-            const textcontent = li.querySelector(".tododesc");
-            const editbtn = li.querySelector("#editbtn");
-            const safebtn = li.querySelector('#safebtn');
-            editbtn.addEventListener('click', function () {
-                edittext.value = textcontent.textContent;
-                edittext.style.display = "inline";
-                textcontent.style.display = "none";
-                editbtn.style.display = "none";
-                safebtn.style.display = "inline";
-            })
-            safebtn.addEventListener('click', function () {
-                textcontent.textContent = edittext.value;
-                edittext.style.display = "none";
-                textcontent.style.display = "inline";
-                safebtn.style.display = "none";
-                editbtn.style.display = "inline";
-            })
+        const edittext = li.querySelector(".edittext");
+        const textcontent = li.querySelector(".tododesc");
+        const editbtn = li.querySelector("#editbtn");
+        const safebtn = li.querySelector('#safebtn');
+        editbtn.addEventListener('click', function () {
+            edittext.value = textcontent.textContent;
+            edittext.style.display = "inline";
+            textcontent.style.display = "none";
+            editbtn.style.display = "none";
+            safebtn.style.display = "inline";
+        })
+        safebtn.addEventListener('click', function () {
+            textcontent.textContent = edittext.value;
+            edittext.style.display = "none";
+            textcontent.style.display = "inline";
+            safebtn.style.display = "none";
+            editbtn.style.display = "inline";
+        })
 
-            return li;
-        } else {
-            warning.innerHTML = "Filling each box is compulsary!";
-        }
+        return li;
+    } else {
+        warning.innerHTML = "Filling each box is compulsary!";
     }
+}
 
-    function todoitem(tasks, date, description) {
-        const tasklist = document.getElementById('taskList');
-        const newitem = addTask(tasks, date, description);
-        tasklist.appendChild(newitem);
-    }
+function todoitem(tasks, date, description) {
+    const tasklist = document.getElementById('taskList');
+    const newitem = addTask(tasks, date, description);
+    tasklist.appendChild(newitem);
+}
 
-    document.getElementById('todo-box').addEventListener('submit', function (e) {
-        e.preventDefault();
-        const task = document.getElementById("input-task");
-        const description = document.getElementById("dis");
-        const date = document.getElementById("input-date");
+document.getElementById('todo-box').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const task = document.getElementById("input-task");
+    const description = document.getElementById("dis");
+    const date = document.getElementById("input-date");
 
-        todoitem(task.value,date.value,description.value);
-        task.value = "";
-        description.value = "";
-        date.value = d;
-    });
+    todoitem(task.value, date.value, description.value);
+    task.value = "";
+    description.value = "";
+    date.value = d;
+});
 
-    function deleteTask(button) {
-        const taskItem = button.parentElement;
-        taskItem.remove();
-    }
+function deleteTask(button) {
+    const taskItem = button.parentElement;
+    taskItem.remove();
+}
